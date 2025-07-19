3f1d93a4215fbd2bc98c5704844baf321d9f545b
Ady Aprilriyan Rizki <adyaprilriyanrizki@gmail.com>
2025-07-19 15:00:08 +0700

    update page

--- app/employee/update/[id]/page.tsx
+++ app/employee/update/[id]/page.tsx
@@ -34,13 +34,15 @@ export default function UpdatePage() {
   };
 
   return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Update Employee</h1>
+    <main className="max-w-2xl mx-auto p-6 mt-20">
+      <h1 className="text-2xl font-bold mb-4 bg bg-gray-900 p-2 text-white">Update Employee</h1>
       <form onSubmit={handleUpdate} className="space-y-4">
         <input className="border px-3 py-2 w-full" value={form.name} onChange={e => setForm({ ...form, name: e.target.value })} />
         <input className="border px-3 py-2 w-full" value={form.position} onChange={e => setForm({ ...form, position: e.target.value })} />
         <input className="border px-3 py-2 w-full" value={form.salary} type="number" onChange={e => setForm({ ...form, salary: e.target.value })} />
-        <button className="bg-blue-600 text-white px-4 py-2 rounded">Update</button>
+        <div className='flex justify-end'>
+          <button className="bg-green-600 font-bold text-white px-4 py-2 rounded">Update</button>
+        </div>
       </form>
     </main>
   );
